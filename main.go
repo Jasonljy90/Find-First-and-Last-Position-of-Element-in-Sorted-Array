939d7a3ed25d1cc79d7bd38d5bdfe31059f5daf1
Jason Lee Jian Ying <79128355+Jasonljy90@users.noreply.github.com>
2021-06-30 16:43:17 +0800

    Upload Code

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func searchRange(nums []int, target int) []int {
+	var x []int
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == target {
+			x = append(x, i)
+		}
+	}
+
+	if len(x) == 0 {
+		x = append(x, -1)
+		x = append(x, -1)
+	} else if len(x) == 1 {
+		x = append(x, x[0])
+	}
+	x1 := x[:1]
+	x2 := x[len(x)-1]
+	x3 := append(x1, x2)
+	return x3
+}
+
+func main() {
+	nums := []int{}
+	target := 5
+	result := searchRange(nums, target)
+	fmt.Println(result)
+}
